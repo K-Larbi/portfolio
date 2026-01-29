4452bb3c6e7dbb852fbb77d4a79dccd75d5aa217
Joseph Kwabena Larbi <josephkl264@gmail.com>
2026-01-29 14:51:28 +0000

    Add CORS middleware

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ import os
 load_dotenv()
 
 app = FastAPI()
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # we’ll restrict later
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 client = MongoClient(os.getenv("MONGO_URI"))
 db = client.portfolio_db
@@ -18,3 +28,5 @@ def root():
 def get_projects():
     projects = list(db.projects.find({}, {"_id": 0}))
     return projects
+
+
